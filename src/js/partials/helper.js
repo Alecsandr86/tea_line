c77a8fc76b62bff6ad3fc8704ea99cdf8c0b7420
Alecsandr86 <alecsandrcherneckiy@gmail.com>
2016-01-22 16:01:58 +0200

    header

--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -1,10 +1,69 @@
 //Так приятней :)
-window.log = function(param){
-    console.log(param);
-};
+//window.log = function(param){
+//    console.log(param);
+//};
 $(document).ready(function(){
-    $('.icon').bind('click',function(){
-        alert('Все работает!!! :)')
-    })
+    /********************************
+     *   Слайдер-Карусель Swiper
+     ********************************/
+
+
+    var slider1 = '#slid-1';
+    var slider2 = '#slid-2';
+    var slider3 = '#slid-3';
+
+    var mySwiper1 = new Swiper (slider1, {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        speed: 700,
+        //autoplay: 1000,
+        followFinger:true,
+
+        // If we need pagination
+        pagination: '.slid-1-pages',
+        paginationClickable: true,
+
+        mousewheelControl: true
+
+    });
+
+    var mySwiper2 = new Swiper (slider2, {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        speed: 700,
+        //autoplay: 1000,
+        followFinger:true,
+
+        // If we need pagination
+        pagination: '.slid-2-pages',
+        paginationClickable: true,
+
+        mousewheelControl: true
+
+    });
+
+    var mySwiper3 = new Swiper (slider3, {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        speed: 700,
+        //autoplay: 1000,
+        followFinger:true,
+
+        // If we need pagination
+        pagination: '.slid-3-pages',
+        paginationClickable: true,
+
+        mousewheelControl: true
+
+    });
+
+
+
+    /********************************
+    *   / Слайдер-Карусель Swiper/
+    ********************************/
 
 });
--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@
  Third party
  */
 //= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/Swiper/dist/js/swiper.jquery.min.js
 
 /*
     Custom
